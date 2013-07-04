4531eb1ef646fa91fadc916d98fa28e834d35b23
alrouen <alrouen@live.fr>
2013-07-04 19:20:55 +0200

    angular : update method in the controller

--- app/assets/scripts/app/user/controllers/userTable.js
+++ app/assets/scripts/app/user/controllers/userTable.js
@@ -3,7 +3,7 @@ angular.module('user').controller('userTable', function($scope, userService) {
     $scope.newUser = undefined;
     $scope.isLoading = false;
 
-    $scope.refreshUserTable = function() {
+    var refresh = function() {
         $scope.isLoading = true;
         userService.query(function(allUsers) {
             $scope.users = allUsers;
@@ -11,5 +11,17 @@ angular.module('user').controller('userTable', function($scope, userService) {
         })
     }
 
-    $scope.refreshUserTable();
+    $scope.refreshUserTable = function(){refresh()}
+
+    $scope.deleteUser = function(id) {
+        /*
+        userService.remove(id,refreshUserTable())
+        */
+    }
+
+    $scope.updateUser = function(user) {
+        userService.update({id: user.id}, {name: user.name, email: user.email}, refresh)
+    }
+
+    refresh();
 });
